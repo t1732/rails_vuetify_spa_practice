6c5af91ee67559e64eb1986c19815ea41adbe889
t1732 <oo1732oo@gmail.com>
2018-10-21 12:38:16 +0900

    [chore] add typescript
    rails webpacker:install:typescript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const typescript =  require('./loaders/typescript')
 const vue =  require('./loaders/vue')
 
+environment.loaders.append('typescript', typescript)
 environment.loaders.append('vue', vue)
 
 environment.config.merge({
