1bd7238ced2995390dda0fb098d8574bfa06eb88
t1732 <oo1732oo@gmail.com>
2019-03-14 18:10:25 +0900

    [chore] remove babel-polyfill

--- app/frontend/packs/application.ts
+++ app/frontend/packs/application.ts
@@ -6,7 +6,6 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-import 'babel-polyfill'
 import 'reflect-metadata'
 import 'typeface-roboto'
 import 'material-icons/iconfont/material-icons.css'
