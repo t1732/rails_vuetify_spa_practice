2093802916636222de35c57294135b520da7584b
t1732 <oo1732oo@gmail.com>
2019-03-19 22:07:00 +0900

    [chore] yarn add license-inf-webpack-plugin

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const LicenseInfoWebpackPlugin = require('license-info-webpack-plugin').default
+environment.plugins.prepend('LicenseInfoWebpackPlugin', new LicenseInfoWebpackPlugin({
+  glob: '{LICENSE,license,License}*',
+  outputType: 'banner'
+}))
+
 module.exports = environment.toWebpackConfig()
